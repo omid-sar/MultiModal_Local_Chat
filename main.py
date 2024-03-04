e28001605b4c3b375b6df29d84e08c62519066e0
omid-sar <mr.omid.sardari@gmail.com>
2024-03-04 15:38:18 -0500

    add a new look to file

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from pathlib import Path
 from RagNavigator.logging import logger
+from RagNavigator.utils.common import create_directories
 
 logger.info("Welcome to my custome log")
 from RagNavigator.config.configuration import ConfigurationManager 
 
+config = ConfigurationManager()
+pdf_processing_config = config.get_pdf_processing_config()
+pdf_processing_config.processed_directory
+processed_directory = Path(pdf_processing_config.processed_directory)
+create_directories(processed_directory)
