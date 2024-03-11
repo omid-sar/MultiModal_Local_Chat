bd2ef70311b811926c3e3c04dc5e3bdb88f20085
omid-sar <mr.omid.sardari@gmail.com>
2024-03-10 20:23:12 -0400

    Load libraries

---
+++ src/Multimodal/components/llm_chains.py
@@ -0,0 +1,12 @@
+import chromadb
+from langchain.chains import LLMChain
+from langchain.chains.retrieval_qa.base import RetrievalQA
+from langchain_community.embeddings import HuggingFaceInstructEmbeddings
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import CTransformers
+from langchain_community.vectorstores import Chroma
+from langchain_community.llms import Ollama
+
+from Multimodal.components.prompt_templates import memory_prompt_template
+from utils import load_config
---
+++ src/Multimodal/components/prompt_templates.py
@@ -0,0 +1,1 @@
+memory_prompt_template = ""
